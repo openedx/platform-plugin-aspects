1fbe85c803356e338a05742b03724e503ddfb2da
Cristhian Garcia <crisgarta8@gmail.com>
2024-10-04 13:52:03 -0500

    chore: bump version to v0.11.2

--- platform_plugin_aspects/__init__.py
+++ platform_plugin_aspects/__init__.py
@@ -5,6 +5,6 @@ Aspects plugins for edx-platform.
 import os
 from pathlib import Path
 
-__version__ = "0.11.1"
+__version__ = "0.11.2"
 
 ROOT_DIRECTORY = Path(os.path.dirname(os.path.abspath(__file__)))
